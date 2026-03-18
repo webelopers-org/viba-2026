ea02d9ce84902fbc08105d3666c06890019346b7
Piero Trigoso <trigosop@gmail.com>
2026-03-18 02:22:33 -0300

    chore: update Next.js config for standalone output
    Added output configuration and experimental tracing includes.

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  output: 'standalone',
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
     return webpackConfig
+  },
+  experimental: {
+    outputFileTracingIncludes: {
+      '/*': ['./src/collections/**/*', './src/globals/**/*'],
+    },
   },
 }
 
